0a1ded90e1dc653a5134cff5049471d2afddf2cd
성기동 <sgd0947@gmail.com>
2021-04-10 15:17:19 +0900

    docs: prettier 적용관련 문제발견
    prettier 적용 후 기존 그누보드 코드의 실행과정에서 에러가 발견하여 prettier 코드만 작성 후 미적용상태

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 120, //  줄 바꿈 할 폭 길이
+  tabWidth: 2, // 탭 너비 
+  useTabs: false, // 탭 사용 여부
+  semi: true, // 세미콜론 사용 여부
+  singleQuote: true, // single 쿼테이션 사용 여부
+  proseWrap: "never",
+  arrowParens: "avoid",
+};
